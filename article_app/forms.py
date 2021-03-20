77cf961e6b4531e238c55c06930acdf38746ca05
tanaki93 <tanaki.1609@gmail.com>
2021-03-20 16:23:26 +0600

    added login.html

--- article_app/forms.py
+++ article_app/forms.py
@@ -1,4 +1,6 @@
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm, TextInput, Textarea, Select
 
 from article_app.models import Article, Comment
 
@@ -8,8 +10,30 @@ class ArticleForm(ModelForm):
         model = Article
         fields = 'title text category'.split()
 
+        widgets = {
+            'title': TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Название поста'
+                }
+            ),
+            'text': Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Текст'
+                }
+            ),
+            'category': Select(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Категория'
+                }
+            ),
+        }
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = 'text'.split()
+
